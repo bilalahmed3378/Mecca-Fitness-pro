5871bd4d33f975348fc198b6525ec09013fa2468
Hashim Khan <devhashmi00@gmail.com>
2022-05-09 17:41:33 +0500

    service selection logic done

---
+++ MeccaFitnessPro/Networking/Services Api/Add Service Api/Models/Request Models/AddServiceRequestModel.swift
@@ -0,0 +1,26 @@
+//
+//  AddServiceRequestModel.swift
+//  MeccaFitnessPro
+//
+//  Created by CodeCue on 09/05/2022.
+//
+
+import Foundation
+
+struct AddServiceRequestModel : Encodable {
+    
+    let services : [AddServiceObject]
+    
+}
+
+
+struct AddServiceObject : Encodable , Hashable {
+    
+    let service_id : Int
+    let experience : String
+    let name : String
+    let isPremium : Bool
+    let price : Int
+
+    
+}
